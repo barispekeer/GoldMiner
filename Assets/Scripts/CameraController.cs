5e9864868306bac18256a5df2d9c2237f8d95092
barispekeer <barispekeer@hotmail.com>
2022-11-16 17:47:27 +0300

    The project is finished.

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,19 +4,38 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    //GameObject player;
+    //float speed = 5;
+    float hassasiyet = 1.5f;
+    float yumusaklik = 2f;
+    Vector2 gecisPos, camPos;
     GameObject player;
-    float speed = 5;
+    PlayerController pc;
     void Start()
     {
+        //player = transform.parent.gameObject;
         player = transform.parent.gameObject;
+        pc = GameObject.Find("Player").GetComponent<PlayerController>();
     }
 
     
     void Update()
     {
-        float xMouse = Input.GetAxis("Mouse X");
-        float yMouse = Input.GetAxis("Mouse Y");
-        player.transform.Rotate(0, xMouse*speed, 0,Space.World);
-        player.transform.Rotate(yMouse*speed, 0, 0,Space.World);
+        //float xMouse = Input.GetAxis("Mouse X");
+        //float yMouse = Input.GetAxis("Mouse Y");
+        //player.transform.Rotate(0, xMouse*speed, 0,Space.World);
+        //player.transform.Rotate(yMouse*speed, 0, 0,Space.World);
+        if (pc.gameActive)
+        {
+            Vector2 farePos = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+            farePos = Vector2.Scale(farePos, new Vector2(hassasiyet * yumusaklik, hassasiyet * yumusaklik));
+            gecisPos.x = Mathf.Lerp(gecisPos.x, farePos.x, 1f / yumusaklik);
+            gecisPos.y = Mathf.Lerp(gecisPos.y, farePos.y, 1f / yumusaklik);
+            camPos += gecisPos;
+
+            transform.localRotation = Quaternion.AngleAxis(-camPos.y, Vector3.right);
+            player.transform.localRotation = Quaternion.AngleAxis(camPos.x, player.transform.up);
+        }
+        
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,20 +1,66 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
-    float speed = 0.25f;
+    public TMP_Text scoreTxt, timeTxt;
+    public Image pnl;
+    Rigidbody rigi;
+    float speed = 0.1f;
+    float time = 10f;
+    int score = 0;
+    public bool gameActive = false;
     void Start()
     {
-
+        gameActive = true;
+        rigi = GetComponent<Rigidbody>();
+        scoreTxt.text = "GOLD: " + score + " / 4";
     }
 
 
     void Update()
     {
-        float yatay = Input.GetAxis("Horizontal");
-        float dikey = Input.GetAxis("Vertical");
-        transform.Translate(-yatay * speed, 0, -dikey * speed);
+        time -= Time.deltaTime;
+        timeTxt.text = "TIME: " + (int)time;
+        if (gameActive)
+        {
+            if (time > 0)
+            {
+                float yatay = Input.GetAxis("Horizontal");
+                float dikey = Input.GetAxis("Vertical");
+                transform.Translate(yatay * speed, 0, dikey * speed);
+            }
+            else
+            {
+                gameActive = false;
+                pnl.gameObject.SetActive(true);
+            }
+        }
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag.Equals("Coin"))
+        {
+            score++;
+            scoreTxt.text = "GOLD: " + score + " / 4";
+            Destroy(other.gameObject);
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag.Equals("Respawn"))
+        {
+            pnl.gameObject.SetActive(true);
+            gameActive = false;
+        }
+    }
+    public void Restart()
+    {
+        SceneManager.LoadScene(0);
     }
 }
